35940437bd24f92258fde9a1d1d4cd88fb3f1fcb
oswin902m <1665434994@qq.com>
2025-08-28 10:14:31 +0800

    chat16 metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
 import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
+import { Metadata } from "next";
 
-export default function RootLayout({
-  children,
-}: {
+// https://nextjs.org/learn/dashboard-app/adding-metadata
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
+type Props = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
